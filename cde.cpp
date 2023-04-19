0a355f8e0f046d04fd74451647f39aa5e2aef990
spotivyie <131213609+spotivyie@users.noreply.github.com>
2023-04-18 22:15:19 -0300

    opção de saída
    incluso no sistema a opção de saída para o usuário

--- cde.cpp
+++ cde.cpp
@@ -126,6 +126,7 @@ int main()
 		printf("\t1 - Registrar nomes\n\n");
 		printf("\t2 - Consultar nomes\n\n");
 		printf("\t3 - Deletar nomes\n\n");	
+		printf("\t4 - Sair do sistema \n\n");
 		printf("Opção: "); //fim do menu
 	 
 		scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -146,6 +147,11 @@ int main()
 			deletar();
 			break;
 			
+			case 4:
+			printf("Obrigado por utilizar o sistema \n");
+			return 0;
+			break;
+						
 			default:
 			printf("Essa opção não está disponível\n");
 			system("pause");
